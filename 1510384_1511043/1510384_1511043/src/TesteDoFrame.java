13b3c3b03118292e310a4334660d226841ca94b4
Felipe Viberti <felipegus8@yahoo.com.br>
2017-10-17 11:49:50 -0200

    Start Eclipse Project

---
+++ 1510384_1511043/1510384_1511043/src/TesteDoFrame.java
@@ -0,0 +1,11 @@
+
+public class TesteDoFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame f = new Frame();
+		f.setTitle("Teste Frame");
+		f.setVisible(true);
+	}
+
+}
